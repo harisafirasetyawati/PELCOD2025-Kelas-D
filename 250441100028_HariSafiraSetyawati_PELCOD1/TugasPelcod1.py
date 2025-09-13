25f9a5713e92d87707b4126975276ee9571beb76
HariSafiraSetyawati <harisafirasetyawati@gmail.com>
2025-09-13 22:58:27 +0700

    new file:   250441100028_HariSafiraSetyawati_PELCOD1/TugasPelcod1.py 	new file:   250441100028_HariSafiraSetyawati_PELCOD1/TugasPelcod2.py 	new file:   250441100028_HariSafiraSetyawati_PELCOD1/TugasPelcod3.py

---
+++ 250441100028_HariSafiraSetyawati_PELCOD1/TugasPelcod1.py
@@ -0,0 +1,10 @@
+#tugas ke-1
+nama = "Hari Safira Setyawati"
+umur = 18
+tinggi_badan = 169.0
+angka_favorit = 26
+
+print ("nama", nama)
+print ("umur", umur)
+print ("tinggi_badan", tinggi_badan)
+print ("angka_favorit", angka_favorit)
---
+++ 250441100028_HariSafiraSetyawati_PELCOD1/TugasPelcod3.py
@@ -0,0 +1,8 @@
+#tugas ke-3
+
+angka_favorit = 26
+angka_favorit = int(input(26))
+if angka_favorit % 2
+    print ("angka genap")
+else
+    print ("angka ganjil")
