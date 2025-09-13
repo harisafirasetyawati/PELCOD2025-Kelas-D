e597ca9849c0bf7999af2b7565a06e5276b940cb
HariSafiraSetyawati <harisafirasetyawati@gmail.com>
2025-09-13 23:02:08 +0700

    modified:   250441100028_HariSafiraSetyawati_PELCOD1/TugasPelcod3.py

--- 250441100028_HariSafiraSetyawati_PELCOD1/TugasPelcod3.py
+++ 250441100028_HariSafiraSetyawati_PELCOD1/TugasPelcod3.py
@@ -1,8 +1,7 @@
 #tugas ke-3
 
 angka_favorit = 26
-angka_favorit = int(input(26))
-if angka_favorit % 2
+if angka_favorit % 2 == 0 :
     print ("angka genap")
-else
+else:
     print ("angka ganjil")
