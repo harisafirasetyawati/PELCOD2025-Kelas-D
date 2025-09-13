919e86e23c8b9bf485fc1d462f61a7538b6d08ba
HariSafiraSetyawati <harisafirasetyawati@gmail.com>
2025-09-13 21:55:44 +0700

    new file:   250441100028_HariSafiraSetawati.py

---
+++ 250441100028_HariSafiraSetawati/Percobaan Coding 1.py
@@ -0,0 +1,27 @@
+# penjumlahan
+
+apel = 6
+mangga = 4
+
+print(apel+mangga)
+
+# pengurangan
+
+anggur = 10
+manggis = 3
+
+print(anggur-manggis)
+
+#perkalian
+
+kambing = 3
+kucing = 3
+
+print(kambing*kucing)
+
+#pembagian
+
+kelinci = 10
+sapi = 2
+
+print(kelinci/sapi)
